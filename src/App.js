972d3c8746fbecd306a799826f5f6eb8d910d022
joao <joao.lobom@gmail.com>
2021-07-04 17:06:41 +0100

    Login folder structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React from 'react'
 import './App.css'
 
 function App() {
   return (
-    <div>
-      <p>Clean React App</p>
+    <div className='App'>
     </div>
   )
 }
---
+++ src/pages/Login/Login.js
@@ -0,0 +1,14 @@
+import Accept from '../../components/Buttons/Accept'
+import MetaMaskLogin from '../../components/Buttons/MetaMaskLogin'
+import WalletInput from '../../components/inputs/WalletInput'
+import card from './components/LoginCard'
+
+const Login = () => {
+	return (
+		<div>
+			
+		</div>
+	)
+}
+
+export default Login
