f05b5ae806cec58670f93ea445c31a9745b37857
joao <joao.lobom@gmail.com>
2021-07-13 19:43:56 +0100

    Simple body formatting, and todo lists

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -5,7 +5,8 @@ import BodyParticipate from './components/BodyParticipate'
 import Footer from './components/Footer'
 import "./LandingPage.css"
 
-//TODO: put types of components in folders
+//TODO: put types of components in folders. Add text and formatting to the rest of the body. Pure styling
+
 const Login = () => {
   return (
     <div className='container'>
