27e359ebbc2891287dda3c5ab3640c450d70f76b
joao <joao.lobom@gmail.com>
2021-07-15 17:38:11 +0100

    Small refactor of metamask animation

--- src/components/Cards/LoginCard.js
+++ src/components/Cards/LoginCard.js
@@ -5,28 +5,31 @@ const ModelViewer = require("@metamask/logo");
 class LoginCard extends React.Component {
   componentDidMount() {
     // Extract to seperate component this logic
-        const AnimatedMetamaskLogo = ModelViewer({
-          pxNotRatio: true,
-          width: 110,
-          height: 110,
-          followMouse: false,
-          slowDrift: false,
-        });
-        AnimatedMetamaskLogo.setFollowMouse(true);
-        const container = document.getElementById("logo-container");
-        container.prepend(AnimatedMetamaskLogo.container);
+    metamaskAnimation();
   }
   render() {
     return (
-        <div className="metamask-container">
-          <div className="metamask-card">
-            <div className="metamask-title">Click to login with metamask</div>
-            <div id="logo-container" className="metamask-logo">
-            </div>
-          </div>
+      <div className="metamask-container">
+        <div className="metamask-card">
+          <div className="metamask-title">Click to login with metamask</div>
+          <div id="logo-container" className="metamask-logo"></div>
         </div>
+      </div>
     );
   }
+}
+
+function metamaskAnimation() {
+  const AnimatedMetamaskLogo = ModelViewer({
+    pxNotRatio: true,
+    width: 110,
+    height: 110,
+    followMouse: false,
+    slowDrift: false,
+  });
+  AnimatedMetamaskLogo.setFollowMouse(true);
+  const container = document.getElementById("logo-container");
+  container.prepend(AnimatedMetamaskLogo.container);
 }
 
 export default LoginCard;
