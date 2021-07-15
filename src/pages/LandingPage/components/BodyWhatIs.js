baa8af5cedc334901d88862b9fb28702c72236a5
joao <joao.lobom@gmail.com>
2021-07-15 18:29:16 +0100

    h1 refactor

--- src/pages/LandingPage/components/BodyWhatIs.js
+++ src/pages/LandingPage/components/BodyWhatIs.js
@@ -5,6 +5,7 @@ const BodyWhatIs = () => {
     <div className='body-WhatIs-content'>
       <h1>What is decentralization</h1>
       <h2>Future of organizations</h2>
+      <h3>Why taxation if theft</h3>
     </div>
   );
 };
