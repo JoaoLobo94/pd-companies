1a498c6affb33b9c1fea6f68e2d9d5f36496862f
joao <joao.lobom@gmail.com>
2021-07-17 17:47:05 +0100

    Implement user dashboard

---
+++ src/pages/UserAreaDashboard/UserDashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './UserDashboard.css'
+
+const UserDashboard = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
+};
+
+export default UserDashboard;
