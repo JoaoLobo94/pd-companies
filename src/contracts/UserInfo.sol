cd92c8726e3aa8e1f5caec7778059c885488a02a
joao <joao.lobom@gmail.com>
2021-08-01 15:11:47 +0100

    Update user contract

--- src/contracts/UserInfo.sol
+++ src/contracts/UserInfo.sol
@@ -1,6 +1,6 @@
 pragma solidity >=0.4.22 <0.9.0;
 
-contract userInfo {
+contract UserInfo {
     struct User {
         uint256 id;
         string name;
@@ -32,40 +32,56 @@ contract userInfo {
         nextId++;
     }
 
-    function read(uint256 id) view public returns(uint256, string memory, string memory, string memory, uint8, address){
-	    for (uint256 i = 0; i < users.length; i++) {
-		    if (users[i].id == id) {
-			   return(users[i].id, users[i].name, users[i].job, users[i].jobType, users[i].reputation, users[i].account);
-		    }
-			   
-		    
-	    }
+    function read(uint256 id)
+        public
+        view
+        returns (
+            uint256 identification,
+            string memory name,
+            string memory job,
+            string memory jobtype,
+            uint8 reputation,
+            address account
+        )
+    {
+        for (uint256 i = 0; i < users.length; i++) {
+            if (users[i].id == id) {
+                return (
+                    users[i].id,
+                    users[i].name,
+                    users[i].job,
+                    users[i].jobType,
+                    users[i].reputation,
+                    users[i].account
+                );
+            }
+        }
     }
 
     //TODO: update functionality for all other params of the struct
 
     function updateAccount(uint256 id, address account) public {
-	    for (uint256 i = 0; i < users.length; i++) {
-		    if (users[i].id == id) {
-			    users[i].account = account;
-		    }
-	    }
+        for (uint256 i = 0; i < users.length; i++) {
+            if (users[i].id == id) {
+                users[i].account = account;
+            }
+        }
     }
 
     function updateJob(uint256 id, string memory job) public {
-	    for (uint256 i = 0; i < users.length; i++) {
-		    if (users[i].id == id) {
-			    users[i].job = job;
-		    }
-	    }
+        for (uint256 i = 0; i < users.length; i++) {
+            if (users[i].id == id) {
+                users[i].job = job;
+            }
+        }
     }
 
     function updateReputation(uint256 id, uint8 reputation) public {
-	    for (uint256 i = 0; i < users.length; i++) {
-		    if (users[i].id == id) {
-			    users[i].reputation = reputation;
-		    }
-	    }
+        for (uint256 i = 0; i < users.length; i++) {
+            if (users[i].id == id) {
+                users[i].reputation = reputation;
+            }
+        }
     }
-  //TODO: implement destroy functionality if a user wants to quit
+    //TODO: implement destroy functionality if a user wants to quit
 }
