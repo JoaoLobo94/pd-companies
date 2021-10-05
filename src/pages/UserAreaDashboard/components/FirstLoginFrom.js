662c9af26270e2d9994d9b767d2c59f70f99f646
joao <joao.lobom@gmail.com>
2021-10-05 10:12:54 +0200

    remove vscode

---
+++ src/pages/UserAreaDashboard/components/FirstLoginFrom.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const FirstLoginFrom = () => {
+	return (
+		<div>
+			<h3>What is your name?</h3>
+			<input type="text" />
+			<h3>Do you want to start a company, or work?</h3>
+			<input type="text" />
+			<h3>What job do you do?</h3>
+			<input type="text" />
+		</div>
+	)
+}
+
+export default FirstLoginFrom
